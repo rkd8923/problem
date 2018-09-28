7e80730b68c4c8ddf0f1cdd4f718e91cdf64b6f7
Jiwon Kang <rkd8923@gmail.com>
2018-09-28 19:59:33 +0900

    m 1463

--- 1463.py
+++ 1463.py
@@ -1,22 +1,21 @@
 N = int(input())
-
-def check(n, l):
+l = []
+def check(n):
     if n==1:
-        l[n] = 0
         return 0
-
+    if l[n] > 0:
+        return l[n]
+    l[n] = check(n-1)+1
     if n%3==0:
-        l[n] = min(check(n//3, l)+1, l[n])
+        l[n] = min(check(n//3)+1, l[n])
     if n%2==0:
-        l[n] = min(check(n//2, l)+1, l[n])
-    l[n] = min(check(n-1, l)+1, l[n])
-
+        l[n] = min(check(n//2)+1, l[n])
     return l[n]
 
 def solution(n):
-    l = []
     for x in range(n+1):
-        l.append(n+1)
-    check(n, l)
+        l.append(0)
+    check(n)
     return l[n]
+
 print(solution(N))
